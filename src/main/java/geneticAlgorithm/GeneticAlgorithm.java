ff82feba9f4f990efb15925bdaa89c5e756b17d8
Sachin Dhumne <dhumne.s@husky.neu.edu>
2021-08-04 18:44:38 -0400

    Basic Genetic Algo with 10 Gene Char

--- src/main/java/geneticAlgorithm/GeneticAlgorithm.java
+++ src/main/java/geneticAlgorithm/GeneticAlgorithm.java
@@ -1,4 +1,134 @@
 package geneticAlgorithm;
 
+import java.util.Random;
+
 public class GeneticAlgorithm {
+
+    Population population = new Population();
+    Individual fittest;
+    Individual secondFittest;
+    int generationCount = 0;
+
+    public static void main(String[] args) {
+
+        Random rn = new Random();
+
+        GeneticAlgorithm demo = new GeneticAlgorithm();
+
+        //Initialize population
+        demo.population.initializePopulation(1000);
+
+        //Calculate fitness of each individual
+        demo.population.calculateFitness();
+
+        System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
+
+        //While population gets an individual with maximum fitness
+        while (demo.population.fittest < 8) {
+            ++demo.generationCount;
+
+            //Do selection
+            demo.selection();
+
+            //Do crossover
+            demo.crossover();
+
+            //Do mutation under a random probability
+            if (rn.nextInt()%7 < 5) {
+                demo.mutation();
+            }
+
+            //Add fittest offspring to population
+            demo.addFittestOffspring();
+
+            //Calculate new fitness value
+            demo.population.calculateFitness();
+
+            System.out.println("Generation: " + demo.generationCount + " Fittest: " + demo.population.fittest);
+        }
+
+        System.out.println("\nSolution found in generation " + demo.generationCount);
+        System.out.println("Fitness: "+demo.population.getFittest().fitness);
+        System.out.print("Genes: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(demo.population.getFittest().genes[i]);
+        }
+
+        System.out.println("");
+
+    }
+
+    //Selection
+    void selection() {
+
+        //Select the most fittest individual
+        fittest = population.getFittest();
+
+        //Select the second most fittest individual
+        secondFittest = population.getSecondFittest();
+    }
+
+    //Crossover
+    void crossover() {
+        Random rn = new Random();
+
+        //Select a random crossover point
+        int crossOverPoint = rn.nextInt(population.individuals[0].geneLength);
+
+        //Swap values among parents
+        for (int i = 0; i < crossOverPoint; i++) {
+            char temp = fittest.genes[i];
+            fittest.genes[i] = secondFittest.genes[i];
+            secondFittest.genes[i] = temp;
+
+        }
+
+    }
+
+    //Mutation
+    void mutation() {
+        Random rn = new Random();
+
+        //Select a random mutation point
+        int mutationPoint = rn.nextInt(population.individuals[0].geneLength);
+
+        //Flip values at the mutation point
+        if (fittest.genes[mutationPoint] == 71) {
+            fittest.genes[mutationPoint] = 'A';
+        } else {
+            fittest.genes[mutationPoint] = 'C';
+        }
+
+        mutationPoint = rn.nextInt(population.individuals[0].geneLength);
+
+        if (secondFittest.genes[mutationPoint] == 65) {
+            secondFittest.genes[mutationPoint] = 'T';
+        } else {
+            secondFittest.genes[mutationPoint] = 'G';
+        }
+    }
+
+    //Get fittest offspring
+    Individual getFittestOffspring() {
+        if (fittest.fitness > secondFittest.fitness) {
+            return fittest;
+        }
+        return secondFittest;
+    }
+
+
+    //Replace least fittest individual from most fittest offspring
+    void addFittestOffspring() {
+
+        //Update fitness values of offspring
+        fittest.calcFitness();
+        secondFittest.calcFitness();
+
+        //Get index of least fit individual
+        int leastFittestIndex = population.getLeastFittestIndex();
+
+        //Replace least fittest individual from most fittest offspring
+        population.individuals[leastFittestIndex] = getFittestOffspring();
+    }
+
 }
---
+++ src/main/java/geneticAlgorithm/Individual.java
@@ -0,0 +1,37 @@
+package geneticAlgorithm;
+
+import java.util.Random;
+
+public class Individual {
+
+        int fitness = 0;
+        String geneType = "ACGT";
+        char[] genes = new char[10];
+        int geneLength = 10;
+
+        public Individual() {
+
+            Random rn = new Random();
+
+            //Set genes randomly for each individual
+            for (int i = 0; i < genes.length; i++) {
+                int randIdx = new Random().nextInt(geneType.length());
+                char randChar = geneType.charAt(randIdx);
+                genes[i] = randChar;
+            }
+
+            fitness = 0;
+        }
+
+        //Calculate fitness
+        public void calcFitness() {
+
+            fitness = 0;
+            for (int i = 0; i < 10; i++) {
+                if (genes[i] == 71) {
+                    ++fitness;
+                }
+            }
+        }
+
+}
